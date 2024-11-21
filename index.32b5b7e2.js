ac072d96533314ce60579e7afb869f4e25d4d0c1
D0dgerJ <d0dgerj89@gmail.com>
2024-11-21 22:46:15 +0200

    make build

---
+++ index.32b5b7e2.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var r=t.target.closest(".list-item");if(r){t.preventDefault();var l=r.querySelector(".list-item__link");l&&(e.src=l.href)}});
+//# sourceMappingURL=index.32b5b7e2.js.map
